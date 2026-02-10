d8a8c89e5e9806a81becfe454add8bb2dcebabab
Ergest <ergestbejdaj0@gmail.com>
2026-02-10 10:15:12 +0100

    New

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
           <Row>
             <Col lg="12" className="text-center mb-5">
               <h6 className="section__subtitle">Come with</h6>
-              <h2 className="section__title">Hot Offers</h2>
+              <h2 className="section__title">Hott Offers</h2>
             </Col>
 
             {carData.slice(0, 14).map((item) => (
